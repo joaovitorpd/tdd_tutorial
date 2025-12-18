204b0f4098f0018428e76db6924f52f1ef3c03d8
joaovitorpd <joaovitorpd@gmail.com>
2025-12-18 10:05:06 -0300

    Update the minimum iOS version to 13.0 and update dependencies in pubspec.yaml.

--- lib/src/authentication/data/models/user_model.dart
+++ lib/src/authentication/data/models/user_model.dart
@@ -12,25 +12,26 @@ class UserModel extends User {
   });
 
   const UserModel.empty()
-  : this(
-    id: '1',
-    createdAt: '_empty.createdAt',
-    name: '_empty.name',
-    avatar: '_empty.avatar',
-  );
+      : this(
+          id: '1',
+          createdAt: '_empty.createdAt',
+          name: '_empty.name',
+          avatar: '_empty.avatar',
+        );
 
   factory UserModel.fromJson(String source) =>
-    UserModel.fromMap(jsonDecode(source) as DataMap);
+      UserModel.fromMap(jsonDecode(source) as DataMap);
 
   UserModel.fromMap(DataMap map)
-    : this(
-        avatar: map['avatar'] as String,
-        id: map['id'] as String,
-        createdAt: map['createdAt'] as String,
-        name: map['name'] as String,
-      );
+      : this(
+          avatar: map['avatar'] as String,
+          id: map['id'] as String,
+          createdAt: map['createdAt'] as String,
+          name: map['name'] as String,
+        );
 
   UserModel copyWith({
+    //Must be nullable because not all fields are required to be updated
     String? avatar,
     String? id,
     String? createdAt,
@@ -40,16 +41,15 @@ class UserModel extends User {
         avatar: avatar ?? this.avatar,
         id: id ?? this.id,
         createdAt: createdAt ?? this.createdAt,
-        name: name?? this.name
-    );
+        name: name ?? this.name);
   }
 
   DataMap toMap() => {
-    'id': id,
-    'avatar': avatar,
-    'createdAt': createdAt,
-    'name': name,
-  };
+        'id': id,
+        'avatar': avatar,
+        'createdAt': createdAt,
+        'name': name,
+      };
 
   String toJson() => jsonEncode(toMap());
 }
